c61fd1c154de7965ce1403a3cdf5cb10036ead4d
mrakakam <johnakakamjohn@gmail.com>
2026-02-23 16:10:54 -0800

    created my Hero.jsx

--- my-project/src/components/Hero.jsx
+++ my-project/src/components/Hero.jsx
@@ -1,2 +1,17 @@
 import heroimg from "../assets/assets/images/illustration-empty.svg"
 import calculatorimg from "../assets/assets/images/icon-calculator.svg"
+
+export const Hero = () =>{
+
+
+
+    return(
+
+        <>
+        
+        
+        
+        
+        </>
+    )
+}
